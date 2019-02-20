b81c8f963aef7f0f5a2eda704ec963c937a38b3f
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-02-20 12:59:38 +0900

    Update android project

--- XFApp/App.xaml.cs
+++ XFApp/App.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
 namespace $safeprojectname$
 {
     public partial class App : Application
